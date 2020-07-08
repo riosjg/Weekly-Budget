c1f4eae3e5dd5f28b29aaee438eb5b4facdbd7c8
Gextox <gabriel_il@live.com>
2020-07-08 01:14:30 -0300

    Updates

---
+++ precache-manifest.ebbe9fde6cb6c52fb5a15f1eefbf7b51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9690d91e60ec33f617713abb3111ad60",
+    "url": "/Weekly-Budget/index.html"
+  },
+  {
+    "revision": "4b7cb38d4a661fb2b77d",
+    "url": "/Weekly-Budget/static/css/main.c3fcd8c2.chunk.css"
+  },
+  {
+    "revision": "784ca5aabfe8d217b974",
+    "url": "/Weekly-Budget/static/js/2.ababce0a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Weekly-Budget/static/js/2.ababce0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b7cb38d4a661fb2b77d",
+    "url": "/Weekly-Budget/static/js/main.596427e1.chunk.js"
+  },
+  {
+    "revision": "2a394d572b910698435f",
+    "url": "/Weekly-Budget/static/js/runtime-main.4cc857f6.js"
+  }
+]);
